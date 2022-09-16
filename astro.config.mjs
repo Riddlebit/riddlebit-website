185827731da624cdc6ad805001918aadd3011df4
Håvard Aasmo <havi95@gmail.com>
2022-09-16 16:01:26 +0200

    Update Astro + fix markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: "https://www.riddlebit.net", // Your public domain. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+
+export default defineConfig({
+  site: "https://www.riddlebit.net",
+  server: {
+    port: 3000,
+    host: true
   },
-  devOptions: {
-    hostname: "0.0.0.0", // The hostname to run the dev server on.
-    port: 3000, // The port to run the dev server on.
-  },
-  renderers: ["@astrojs/renderer-svelte"],
-};
+  integrations: [svelte()]
+});
